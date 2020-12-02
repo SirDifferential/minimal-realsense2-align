9a9503278e9e115c91ecf8ea9fd40a30d52ef74a
Jesse Kaukonen <jesse.kaukonen@gmail.com>
2020-12-02 14:35:13 +0200

    spaces to tabs

--- align-nogui.cpp
+++ align-nogui.cpp
@@ -4,13 +4,13 @@
 #include "rs_settings.h"
 
 enum class direction {
-    to_depth,
-    to_color
+	to_depth,
+	to_color
 };
 
 int main(int argc, char * argv[]) try {
 	rs2::context context;
-    rs2::pipeline pipe(context);
+	rs2::pipeline pipe(context);
 
 	rs2::device_list devs = context.query_devices();
 	if (devs.size() != 1) {
@@ -40,7 +40,7 @@ int main(int argc, char * argv[]) try {
 	std::cout << "loading custom settings" << std::endl;
 	adv.load_json(realsense_advanced_settings_json);
 
-    rs2::config cfg;
+	rs2::config cfg;
 
 	const int cDepthW = 848;
 	const int cDepthH = 480;
@@ -53,38 +53,38 @@ int main(int argc, char * argv[]) try {
 	cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
 	cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F);
 
-    pipe.start(cfg);
+	pipe.start(cfg);
 
-    rs2::align align_to_depth(RS2_STREAM_DEPTH);
-    rs2::align align_to_color(RS2_STREAM_COLOR);
+	rs2::align align_to_depth(RS2_STREAM_DEPTH);
+	rs2::align align_to_color(RS2_STREAM_COLOR);
 
-    //direction dir = direction::to_depth;
+	//direction dir = direction::to_depth;
 	direction dir = direction::to_color;
 
 	int framecount = 0;
 
-    while (true) {
-        rs2::frameset frameset = pipe.wait_for_frames(3000);
+	while (true) {
+		rs2::frameset frameset = pipe.wait_for_frames(3000);
 
-        if (dir == direction::to_depth) {
-            frameset = align_to_depth.process(frameset);
-        } else {
-            frameset = align_to_color.process(frameset);
-        }
+		if (dir == direction::to_depth) {
+			frameset = align_to_depth.process(frameset);
+		} else {
+			frameset = align_to_color.process(frameset);
+		}
 
-        auto depth = frameset.get_depth_frame();
-        auto color = frameset.get_color_frame();
+		auto depth = frameset.get_depth_frame();
+		auto color = frameset.get_color_frame();
 		framecount++;
 		std::cout << "aligned frames: " << framecount << std::endl;
-    }
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 } catch (const rs2::error & e) 
-    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" <<
-		e.get_failed_args() << "):\n    " << e.what() << std::endl;
-    return EXIT_FAILURE;
+	std::cerr << "RealSense error calling " << e.get_failed_function() << "(" <<
+		e.get_failed_args() << "):\n	" << e.what() << std::endl;
+	return EXIT_FAILURE;
 } catch (const std::exception & e) {
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
+	std::cerr << e.what() << std::endl;
+	return EXIT_FAILURE;
 }
 
